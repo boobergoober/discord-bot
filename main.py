ef4d357d56e6f190a067f8deadcbe16ef8d8651b
boobergoober <thegreatboobergoober@gmail.com>
2026-06-25 12:06:04 +0200

    password is 123, should be able to turn server on and off?

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import logging
 from dotenv import load_dotenv
 import os
 import sqlite3
+import mcpq as mc
+import subprocess
+import time
 
 load_dotenv()
 
@@ -14,7 +17,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-
 #prefix
 
 bot = commands.Bot(command_prefix="!", intents= intents)
@@ -22,6 +24,7 @@ bot = commands.Bot(command_prefix="!", intents= intents)
 # Shit-count, restoring the dictionary
     
 dick = {}
+server_on = True
 
 #handling events
 
@@ -34,20 +37,62 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if "shit" in message.content and message.content != "!shit_count":
-        print("doing something?")
-        if message.author.name in dick.keys():
-            dick[message.author.name] += 1    
-        else:
-            dick.update({message.author.name : 1})    
-        print("didnt get stuck")
-
-    print(dick)
-
     await bot.process_commands(message)
 
 @bot.command()
-async def shit_count(ctx):
-    await ctx.send("You said shit {} times !".format(dick[ctx.author.name]))
+async def tries(ctx):
+    await ctx.send(f"You tried the to guess the password {dick[ctx.author.name]} times")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello {} !".format(ctx.author.name))
+
+@bot.command()
+async def whitelist(ctx, operat, user, password):
+    if server_on == False:
+        await ctx.send("server is off")
+    elif operat not in ["add", "remove"]:
+        await ctx.send("typo")
+    elif password == "123":
+        try: mc.runCommand(f"whitelist {operat} {user}")
+        except: await ctx.send("something went wrong with adding the user...")
+    else:
+        await ctx.send("wrong password, don't try again")
+        if ctx.author.name in dick.keys():
+            dick[ctx.author.name] += 1    
+        else:
+            dick.update({ctx.author.name : 1})
+
+@bot.command()
+async def mccommand(ctx, password, command):
+    if server_on == False:
+        await ctx.send("server is off")
+    elif password == 123:
+        mc.runCommand(command)
+    else:
+        await ctx.send("wrong password, don't try again")
+        if ctx.author.name in dick.keys():
+            dick[ctx.author.name] += 1    
+        else:
+            dick.update({ctx.author.name : 1})
+
+@bot.command()
+async def off(ctx):
+    global server_on
+    if server_on == "False":
+        await ctx.send("server is already off!")
+    else:
+        subprocess.Popen(["stop"])
+        server_on = False
+
+@bot.command()
+async def on(ctx):
+    global server_on
+    if server_on == True:
+        await ctx.send("server is already on!")
+    else:
+        subprocess.Popen(["cd", "."])
+        subprocess.Popen(["./start.sh"])
+        server_on = True
 
 bot.run(token, log_handler = handler, log_level=logging.DEBUG)
